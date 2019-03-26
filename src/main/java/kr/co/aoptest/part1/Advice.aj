083d99f09a0cc6948a11095c31f006d6d502725d
봄 <jxxbom@gmail.com>
2019-03-26 18:33:53 +0900

    3/26 create AOP project

---
+++ src/main/java/kr/co/aoptest/part1/Advice.aj
@@ -0,0 +1,5 @@
+package kr.co.aoptest.part1;
+
+public aspect Advice {
+	
+}
---
+++ src/main/java/kr/co/aoptest/part1/Test.java
@@ -0,0 +1,15 @@
+package kr.co.aoptest.part1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		new Test().say();
+		
+	}
+	
+	void say() {
+		System.out.println("어피치");
+	}
+
+}
